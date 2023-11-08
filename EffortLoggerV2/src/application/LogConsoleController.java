3e3e360b8641419e4a74c385ee2a6333e715d1a3
DuyTran67 <88998212+DuyTran67@users.noreply.github.com>
2023-11-08 14:15:31 -0700

    g

--- EffortLoggerV2/src/application/LogConsoleController.java
+++ EffortLoggerV2/src/application/LogConsoleController.java
@@ -1,5 +1,38 @@
 package application;
 
-public class LogConsoleController {
+import javafx.event.ActionEvent;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
 
+public class LogConsoleController {
+	private Stage stage;
+	private Scene scene;
+	private Parent root;
+	
+	public void goToDefectConsole(ActionEvent event) {
+		try {
+			root = FXMLLoader.load(getClass().getResource("/FXML_files/DefectConsole.fxml"));
+			stage = (Stage)((Node)event.getSource()).getScene().getWindow();
+			scene  = new Scene(root);
+			stage.setScene(scene);
+			stage.show();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public void goToEditor(ActionEvent event) {
+		try {
+			root = FXMLLoader.load(getClass().getResource("/FXML_files/EffortLogEditor.fxml"));
+			stage = (Stage)((Node)event.getSource()).getScene().getWindow();
+			scene  = new Scene(root);
+			stage.setScene(scene);
+			stage.show();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
 }
