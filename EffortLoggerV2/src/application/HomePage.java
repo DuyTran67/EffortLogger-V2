47118723b0738b8a4a29e0ee675c4138a41a708e
DuyTran67 <88998212+DuyTran67@users.noreply.github.com>
2023-11-11 20:50:30 -0700

    planning poker wip

--- EffortLoggerV2/src/application/HomePage.java
+++
@@ -1,9 +0,0 @@
-package application;
-
-import javafx.fxml.FXML;
-import javafx.scene.control.Button;
-
-public class HomePage {
-	@FXML
-	private Button effortConsoleButton, editorButton, defectButton, logsButton, definitionsButton, pokerButton;
-}
