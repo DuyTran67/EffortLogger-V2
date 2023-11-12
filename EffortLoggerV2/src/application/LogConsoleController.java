08d55caf08d1a6d799f43b06dd7ab8897d3d5152
TUNG XUAN NGUYEN <135734821+tungnguyenasu@users.noreply.github.com>
2023-11-13 00:44:23 +0700

    Update LogConsoleController.java

--- EffortLoggerV2/src/application/LogConsoleController.java
+++ EffortLoggerV2/src/application/LogConsoleController.java
@@ -1,127 +1,111 @@
-package application;
-
-// Mainline class for the EffortLogger V2 project.
-
-import javafx.application.Application;        
-import javafx.scene.Node;
-import javafx.scene.Scene;
-import javafx.stage.Stage;
-
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.fxml.FXMLLoader;
-import java.io.IOException;
-import javafx.scene.control.*;
-import javafx.scene.Parent;
-import javafx.scene.layout.*;
-import javafx.collections.FXCollections;
-import javafx.geometry.Insets;
+import javafx.scene.control.SplitMenuButton;
 
-public class MainEffortLogger extends Application {
-    private TextArea logArea;
-    private ComboBox<String> projectComboBox;
-    private ComboBox<String> lifeCycleStepComboBox;
-    private ComboBox<String> effortCategoryComboBox;
-    private ComboBox<String> deliverableComboBox;
-    
-    BorderPane root = new BorderPane();
-    root.setPadding(new Insets(10));
+public class LogConsoleController {
 
-    ToolBar toolbar = new ToolBar();
-    Button startActivityButton = new Button("Start an Activity");
-    Button stopActivityButton = new Button("Stop this Activity");
-    toolbar.getItems().addAll(startActivityButton, stopActivityButton);
+    @FXML
+    private SplitMenuButton projectSplitMenuButton;
 
-    root.setTop(toolbar);
+    @FXML
+    private SplitMenuButton lifeCycleSplitMenuButton;
 
-    logArea = new TextArea();
-    logArea.setEditable(false);
-    root.setCenter(logArea);
-    
-    HBox projectBox = new HBox(10);
-    projectBox.setPadding(new Insets(10));
-    Label projectLabel = new Label("Project:");
-    projectComboBox = new ComboBox<>(FXCollections.observableArrayList("", "Project 1", "Project 2", "Project 3"));
-    projectBox.getChildren().addAll(projectLabel, projectComboBox);
+    @FXML
+    private SplitMenuButton effortCategorySplitMenuButton;
 
-    HBox lifeCycleStepBox = new HBox(10);
-    lifeCycleStepBox.setPadding(new Insets(10));
-    Label lifeCycleStepLabel = new Label("Life Cycle Step:");
-    lifeCycleStepComboBox = new ComboBox<>(FXCollections.observableArrayList("", "Design", "Development", "Testing", "Deployment"));
-    lifeCycleStepBox.getChildren().addAll(lifeCycleStepLabel, lifeCycleStepComboBox);
+    @FXML
+    private SplitMenuButton deliverableSplitMenuButton;
 
-    HBox effortCategoryBox = new HBox(10);
-    effortCategoryBox.setPadding(new Insets(10));
-    Label effortCategoryLabel = new Label("Effort Category:");
-    effortCategoryComboBox = new ComboBox<>(FXCollections.observableArrayList("", "High", "Medium", "Low"));
-    effortCategoryBox.getChildren().addAll(effortCategoryLabel, effortCategoryComboBox);
+    // Placeholder methods, you need to implement the actual logic
 
-    HBox deliverableBox = new HBox(10);
-    deliverableBox.setPadding(new Insets(10));
-    Label deliverableLabel = new Label("Deliverable:");
-    deliverableComboBox = new ComboBox<>(FXCollections.observableArrayList("", "Software 1", "Software 2", "Software 3"));
-    deliverableBox.getChildren().addAll(deliverableLabel, deliverableComboBox);
+    @FXML
+    private void startActivity(ActionEvent event) {
+	System.out.println("Activity started");
 
-    VBox sidePanel = new VBox(20);
-    sidePanel.getChildren().addAll(projectBox, lifeCycleStepBox, effortCategoryBox, deliverableBox);
-
-    root.setLeft(sidePanel);
-    
-    startActivityButton.setOnAction(event -> {
-        logActivity();
-    });
-
-    stopActivityButton.setOnAction(event -> {
-        logActivity();
-    });
-    
-    private void logActivity() {
-        String project = projectComboBox.getValue();
-        String lifeCycleStep = lifeCycleStepComboBox.getValue();
-        String effortCategory = effortCategoryComboBox.getValue();
-        String deliverable = deliverableComboBox.getValue();
-
-        // Your logic to log the activity goes here
+        // Load or refresh data for project, life cycle, effort category, deliverable, etc.
+        loadProjects();
+        loadLifeCycles();
+        loadEffortCategories();
+        loadDeliverables();
     }
-    
-	@Override
-	public void start(Stage stage) {
-		try {
-			// starting screen
-			Parent root1 = FXMLLoader.load(getClass().getResource("/FXML_files/LoginPage.fxml"));
-			Scene scene1 = new Scene(root1);
-			stage.setScene(scene1);
-			stage.setTitle("EffortLogger V2");
-			stage.show();
-			// Create buttons for each action
-	        Button effortLogEditorButton = new Button("Effort Log Editor");
-	        Button defectLogConsoleButton = new Button("Defect Log Console");
-	        Button definitionsButton = new Button("Definitions");
-	        Button effortDefectLogsButton = new Button("Effort and Defect Logs");
 
-	        // Set button on action
-	        effortLogEditorButton.setOnAction(event -> System.out.println("Effort Log Editor"));
-	        defectLogConsoleButton.setOnAction(event -> System.out.println("Defect Log Console"));
-	        definitionsButton.setOnAction(event -> System.out.println("Definitions"));
-	        effortDefectLogsButton.setOnAction(event -> System.out.println("Effort and Defect Logs"));
+    @FXML
+    private void stopActivity(ActionEvent event) {
+        // Implement logic to stop an activity and store in the database
+        String stoppedProject = projectSplitMenuButton.getText();
+        String stoppedLifeCycle = lifeCycleSplitMenuButton.getText();
+        String stoppedEffortCategory = effortCategorySplitMenuButton.getText();
+        String stoppedDeliverable = deliverableSplitMenuButton.getText();
 
-	        // Add padding to buttons
-	        effortLogEditorButton.setPadding(new Insets(10, 20, 10, 20));
-	        defectLogConsoleButton.setPadding(new Insets(10, 20, 10, 20));
-	        definitionsButton.setPadding(new Insets(10, 20, 10, 20));
-	        effortDefectLogsButton.setPadding(new Insets(10, 20, 10, 20));
+        // Perform additional validation if needed
 
-	        // Add buttons to a VBox layout
-	        VBox vbox = new VBox(10);
-	        vbox.setPadding(new Insets(20));
-	        vbox.getChildren().addAll(effortLogEditorButton, defectLogConsoleButton, definitionsButton, effortDefectLogsButton);
-	        
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
-	}
+        // Store the stopped activity information in the database
+        storeActivityDataInDatabase(stoppedProject, stoppedLifeCycle, stoppedEffortCategory, stoppedDeliverable);
+
+        // Display a confirmation message
+        showAlert("Activity Stopped", "Activity stopped and logged successfully.");
+    }
+
+    private void storeActivityDataInDatabase(/* pass necessary parameters */) {
+        // Implement logic to store activity data in the database
+    }
 	
-	public static void main(String[] args) {
-		launch(args);
-	}
+	private void showAlert(String title, String content) {
+	        // Helper method to display an alert
+	        Alert alert = new Alert(Alert.AlertType.INFORMATION);
+	        alert.setTitle(title);
+	        alert.setHeaderText(null);
+	        alert.setContentText(content);
+	        alert.showAndWait();
+	    }
+
+    private void loadProjects() {
+        // Simulate fetching projects from the database. Replace this with your actual database logic.
+        List<String> projects = fetchProjectsFromDatabase();
+
+        // Clear existing items in the menu button
+        projectSplitMenuButton.getItems().clear();
+
+        // Populate the menu button with projects
+        for (String project : projects) {
+            MenuItem menuItem = new MenuItem(project);
+            menuItem.setOnAction(event -> handleProjectSelection(project));
+            projectSplitMenuButton.getItems().add(menuItem);
+        }
+    }
+
+    private List<String> fetchProjectsFromDatabase() {
+        // Implement logic to fetch projects from the database
+        // This is a placeholder, replace it with your actual database logic
+        return List.of("Development Project", "Business Project");
+    }
+
+    private void handleProjectSelection(String selectedProject) {
+        // Implement any additional logic when a project is selected
+        System.out.println("Selected Project: " + selectedProject);
+    }
+
+    private void loadLifeCycles() {
+        // Implement logic to load life cycles from the database and populate the lifeCycleSplitMenuButton
+    }
+
+    private void loadEffortCategories() {
+        // Implement logic to load effort categories from the database and populate the effortCategorySplitMenuButton
+    }
+
+    private void loadDeliverables() {
+        // Implement logic to load deliverables from the database and populate the deliverableSplitMenuButton
+    }
+
+    // Add more methods as needed for managing other data
+
+    @FXML
+    private void goToEditor(ActionEvent event) {
+        // Implement logic to navigate to the Effort Log Editor
+    }
+
+    @FXML
+    private void goToDefectConsole(ActionEvent event) {
+        // Implement logic to navigate to the Defect Log Console
+    }
 }
